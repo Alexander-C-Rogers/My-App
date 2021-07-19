bfe2d809a2fdac861d4e360114ff6c97c4acda07
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-19 14:52:35 -0400

    create Square function for index.js ad .square css  rules for index.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
 import './Resources/CSS/index.css';
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+}
